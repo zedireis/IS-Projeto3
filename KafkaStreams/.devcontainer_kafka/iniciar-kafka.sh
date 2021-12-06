17ca7543ae5c0deeebf01d13a8b8789a9a2e4d86
zedireis <57588751+zedireis@users.noreply.github.com>
2021-12-06 15:31:35 +0000

    Versão Base

---
+++ KafkaStreams/.devcontainer_kafka/iniciar-kafka.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Start the first process
+opt/kafka_2.12-2.8.1/bin/zookeeper-server-start.sh /opt/kafka_2.12-2.8.1/config/zookeeper.properties &
+
+sleep 20
+
+# Start the second process
+opt/kafka_2.12-2.8.1/bin/kafka-server-start.sh /opt/kafka_2.12-2.8.1/config/server.properties &
+# Wait for any process to exit
+wait -n
+# Exit with status of process that exited first
+exit $?
